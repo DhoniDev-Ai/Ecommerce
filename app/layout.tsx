802af5eb668bc18629ee34ae29507b64580b881a
Divit <onlyego1043@gmail.com>
2026-07-14 13:43:50 +0530

    added script facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import NextTopLoader from 'nextjs-toploader';
 import { AuthPopup } from "@/components/auth/AuthPopup";
 import { SmoothScroll } from "@/components/providers/SmoothScroll";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -108,10 +109,39 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '27922731947320845');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${montserrat.variable} ${juana.variable} antialiased`}
         suppressHydrationWarning
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=27922731947320845&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
